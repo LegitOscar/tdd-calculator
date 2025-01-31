3e2542b38ff82aac6600b7d67732ae36018514a8
LegitOscar <oscarboerjesen@gmail.com>
2025-01-31 11:21:21 +0100

    First Commit

---
+++ src/test/java/tdd/CalculatorTest.java
@@ -0,0 +1,43 @@
+package tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator instance = new Calculator();
+
+    @Test
+    void TestAdd(){
+        int expected = 4;
+        int actual = instance.add(2,2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void TestSubtract(){
+        int expected = 2;
+        int actual = instance.subtract(4,2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void TestMultiply(){
+        int expected = 4;
+        int actual = instance.multiply(2,2);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void TestDivide(){
+        int expected = 1;
+        int actual = instance.divide(2,2);
+        assertEquals(expected,actual);
+    }
+
+
+
+
+}
